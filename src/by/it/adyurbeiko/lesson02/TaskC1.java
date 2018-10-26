cdab6cd0e959148cb9ccd6dfc536838296925af0
jarlaksy <44301698+jarlaksy@users.noreply.github.com>
2018-10-26 17:11:50 +0300

    Merge branch 'master' of https://github.com/jarlaksy/cs2018-10-18
    # Conflicts:
    #	src/by/it/adyurbeiko/lesson02/TaskA2.java

--- src/by/it/adyurbeiko/lesson02/TaskC1.java
+++ src/by/it/adyurbeiko/lesson02/TaskC1.java
@@ -1,5 +1,7 @@
 package by.it.adyurbeiko.lesson02;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и выводит их сумму на экран в виде
 
@@ -11,7 +13,16 @@ Sum = 60
 
 */
 class TaskC1 {
-
+    public static void main(String[] args) {
+        System.out.println("Ввод");
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int o= a + b;
+        System.out.println("Вывод");
+        System.out.print("Sum = ");
+        System.out.println(o);
+    }
 
 
 
