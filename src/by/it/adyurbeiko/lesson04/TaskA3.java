3e6d9de0846e1c3df6c003170928ee7a00e3a42a
jarlaksy <44301698+jarlaksy@users.noreply.github.com>
2018-11-01 16:19:01 +0300

    Merge branch 'master' of https://github.com/jarlaksy/cs2018-10-18
    # Conflicts:
    #	src/by/it/adyurbeiko/lesson02/TaskA2.java

--- src/by/it/adyurbeiko/lesson04/TaskA3.java
+++ src/by/it/adyurbeiko/lesson04/TaskA3.java
@@ -22,6 +22,22 @@ package by.it.adyurbeiko.lesson04;
  */
 
 
-public class TaskA3 {
+import java.util.Scanner;
 
+public class TaskA3 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
+        if (a==b && b==c)
+        System.out.println(a+" "+b+" "+c);
+        else if (a==b)
+        System.out.println(a+" "+b);
+        else if (b==c)
+        System.out.println(b+" "+c);
+        else if (a==c)
+        System.out.println(a+" "+c);
+
+    }
 }
