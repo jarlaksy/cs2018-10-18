406710722829d95f919ed590fba0e0173ecd60cd
jarlaksy <44301698+jarlaksy@users.noreply.github.com>
2018-11-02 20:22:07 +0300

    Merge branch 'master' of https://github.com/jarlaksy/cs2018-10-18
    # Conflicts:
    #	src/by/it/adyurbeiko/lesson02/TaskA2.java

--- src/by/it/adyurbeiko/lesson05/TaskA1.java
+++ src/by/it/adyurbeiko/lesson05/TaskA1.java
@@ -24,6 +24,21 @@ package by.it.adyurbeiko.lesson05;
 */
 
 
-public class TaskA1 {
+import java.util.Scanner;
 
-}
+public class TaskA1 {
+    public static void main(String[] args) {
+        int [] mas=new int [10];
+        Scanner scanner=new Scanner(System.in);
+        for (int i = 9; i >=0 ; i--) {
+            mas [i]=scanner.nextInt();
+            }
+        for (int element : mas) {
+            System.out.println(element);
+
+        }
+
+        }
+    }
+
+
